c7886d1d88b7fd224483fd6d8ca1bb05e4391712
Katie Lavigne <38912376+katielavigne@users.noreply.github.com>
2021-02-01 13:46:06 -0500

    Create merge_outputs.m
    Code to combine connectivity outputs with tabular data for further analysis.

---
+++ code/merge_outputs.m
@@ -0,0 +1,27 @@
+% VERTEX-WISE STRUCTURAL COVARIANCE: merge outputs
+%   This script combines outputs (network/smoothing/study) from main
+%   script into a behavioural dataset for further analysis. Assumes behavioural
+%   data has same number of rows/same order as outputs, behavioural data is
+%   located in present working directory, and outputs are in a containing
+%   folder called "netfeats".
+
+% Options
+sm = {'10';'20';'40'};
+nets = {'VIS';'SOM'; 'DAN'; 'VAN'; 'LIM'; 'FPN'; 'DMN'};
+study = 'Insight';
+behfile = 'InsightBehData.mat';
+
+% Code
+load(fullfile(loc, behfile))
+
+for i = 1:size(sm,1)
+    for j = 1:size(nets,1)
+        load(fullfile(pwd, 'netfeats', ['vertexConnectivity_' study '_' sm{i} '_' nets{j} '_features.mat']));
+        beh.([nets{j} sm{i} '_modularity']) = gr.Q;
+        beh.([nets{j} sm{i} '_meanPC']) = sum(gr.PC)';
+        beh.([nets{j} sm{i} '_meanStrength']) = sum(gr.S)';
+        beh.([nets{j} sm{i} '_eigenspect']) = gr.ES;
+    end
+end
+
+writetable(beh,fullfile(pwd, 'vertexConnectivity_results.csv'))
