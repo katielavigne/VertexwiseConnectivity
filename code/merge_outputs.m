9fedfd18a32c8a56fd1f24599a63adf2d823b603
Katie Lavigne <38912376+katielavigne@users.noreply.github.com>
2021-02-01 14:31:35 -0500

    Update merge_outputs.m
    Rename table

--- code/merge_outputs.m
+++ code/merge_outputs.m
@@ -17,12 +17,12 @@ load(behfile)
 for i = 1:size(sm,1)
     for j = 1:size(nets,1)
         load(fullfile(pwd, 'netfeats', ['vertexConnectivity_' study '_' sm{i} '_' nets{j} '_features.mat']));
-        beh.([nets{j} sm{i} '_modularity']) = gr.Q;
-        beh.([nets{j} sm{i} '_meanPC']) = sum(gr.PC)';
-        beh.([nets{j} sm{i} '_meanStrength']) = sum(gr.S)';
-        beh.([nets{j} sm{i} '_eigenspect']) = gr.ES;
+        t.([nets{j} sm{i} '_modularity']) = gr.Q;
+        t.([nets{j} sm{i} '_meanPC']) = sum(gr.PC)';
+        t.([nets{j} sm{i} '_meanStrength']) = sum(gr.S)';
+        t.([nets{j} sm{i} '_eigenspect']) = gr.ES;
     end
 end
 
-writetable(beh,fullfile(pwd, 'vertexConnectivity_results.csv'))
+writetable(t,fullfile(pwd, 'vertexConnectivity_results.csv'))
 save('vertexConnectivity_results.mat')
